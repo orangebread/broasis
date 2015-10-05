9ae4fbef0e052f7e4b6e1d886dfa5565df57cd76
orangebread <jeffyuanlee@gmail.com>
2015-10-05 06:22:03 -0400

    sweg

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socketio = require('socket.io');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var port = process.env.PORT || 8080;
 
 var app = express();
-var port = process.env.PORT || 8080;
+var server = app.listen(port);
+var io = socketio.listen(server);
+
+// Run server
+server.listen(port, function() {
+    console.log('Listening on port: ' + port);
+});
+
+io.on('connection', function(socket) {
+    console.log('User connected.');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,12 +36,9 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', routes);
 app.use('/users', users);
-
-app.listen(port, function() {
-  console.log('Listening on port: ' + port);
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
